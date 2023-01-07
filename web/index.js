94c602c1fab3366b315f1d4a6adca68e051fac39
Samito9920 <sajara.dwfsc13@bootcamp.ucamp.io>
2023-01-07 18:38:57 -0500

    un poco del responsive eliminar y editar

--- web/index.js
+++ web/index.js
@@ -1,22 +1,24 @@
-const formulario = document.querySelector("form");
 let table = document.getElementById("target");
-var btnSave = document.getElementById('guardar');
+const btnSave = document.getElementById('guardar');
+const btnSaveNuevo = document.getElementById('guardarNuevo');
 var datos = []
 
 
 function init(){
   if (window.localStorage != "") {
-    //taskList = JSON.parse(window.localStorage.getItem('taskList'));
     listar()
   } else {
     taskList = [];
   }
+  btnSave.disabled= false;
+  btnSaveNuevo.disabled = true;
   btnSave.addEventListener('click', saveTask);
+  btnSaveNuevo.addEventListener('click', nuevo);
   //showList();
 
   console.log("si funciono")
-
 }
+
 
 //Agregar
 function saveTask(){
@@ -71,17 +73,17 @@ function listar(){
                             <th>Nivel</th></tr></theat>`
 
     datos.forEach((element, index) => {
-      var ced = element.CEDULA;
-      table.innerHTML += `<td> ${element.NOMBRE} </td> 
-      <td >${element.APELLIDOS}</td>
-      <td id='txtcedula'>${element.CEDULA}</td>
-      <td>${element.TELEFONO}</td> 
-      <td>${element.FECHA}</td>
-      <td>${element.EMAIL}</td>
-      <td>${element.NOTA}</td> 
-      <td>${element.NIVEL}</td>
+      table.innerHTML += 
+    `<td> ${element.NOMBRE} </td> 
+     <td >${element.APELLIDOS}</td>
+     <td id='txtcedula'>${element.CEDULA}</td>
+     <td>${element.TELEFONO}</td> 
+     <td>${element.FECHA}</td>
+    <td>${element.EMAIL}</td>
+    <td>${element.NOTA}</td>
+    <td>${element.NIVEL}</td>
       <td><button onclick='editar(${index})' class='btn btn-warning'>Editar</button></td>
-      <td><button onclick="eliminar(${index})" class='btn btn-danger'>Eliminar </button></td>`
+     <td><button onclick="eliminar(${index})" class='btn btn-danger'>Eliminar </button></td>`
     });
   }
 }
@@ -97,20 +99,54 @@ function eliminar(posicion){
     }
   }
   datos.splice(indexArray, 1);
-  localStorage.setItem('estudianteregistrado', JSON.stringify(datos))
+  localStorage.setItem('estudianteregistrado', JSON.stringify(datos));
   console.log(datos)
   listar();
 
 }
 
 function editar(posicion){
-  
+  btnSaveNuevo.disabled = false;
+  btnSave.disabled = true;
   for(i =0 ; i<= datos.length; i++){
     if(i == posicion){
-      console.log(datos[i]);
-    }
+      estudianteNUEVO =
+      ({
+        "NOMBRE": document.getElementById("nombreEstudiante").value =datos[i].NOMBRE,
+        "CEDULA": document.getElementById("cedulaEstudiante").value =datos[i].CEDULA, 
+        "TELEFONO": document.getElementById("tlfEstudiante").value =datos[i].TELEFONO,
+        "APELLIDOS": document.getElementById("apellidosEstudiante").value = datos[i].APELLIDOS,
+        "FECHA": document.getElementById("fcNaEstudiante").value =datos[i].FECHA,
+        "EMAIL": document.getElementById("emailEstudiante").value =datos[i].EMAIL,
+        "NIVEL": document.getElementById("nivelEstudiante").value =datos[i].NIVEL,
+        "NOTA": document.getElementById("notaEstudiante").value =datos[i].NOTA
+      })
+      break;
+    };
   }
+}
 
+function nuevo(){
+  datos = JSON.parse(localStorage.getItem("estudianteregistrado"));
+  const estudianteNuevo =
+  ({
+    "NOMBRE": document.getElementById("nombreEstudiante").value,
+    "CEDULA": document.getElementById("cedulaEstudiante").value, 
+    "TELEFONO": document.getElementById("tlfEstudiante").value,
+    "APELLIDOS": document.getElementById("apellidosEstudiante").value,
+    "FECHA": document.getElementById("fcNaEstudiante").value,
+    "EMAIL": document.getElementById("emailEstudiante").value,
+    "NIVEL": document.getElementById("nivelEstudiante").value,
+    "NOTA": document.getElementById("notaEstudiante").value,
+  }); 
+  //console.log(estudianteNuevo)
+  datos[i] = estudianteNuevo;
+  console.log(datos[i]);
+  localStorage.setItem('estudianteregistrado', JSON.stringify(datos));
+  listar();
+  btnSaveNuevo.disabled = true;
+  btnSave.disabled = false;
+ 
 }
 
 init();
